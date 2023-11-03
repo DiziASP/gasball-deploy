86b8a6d4c8bf9a3a06d1798446bd91335b528f54
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-04 05:38:33 +0700

    feat: lint

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
 
 export async function GET(request: Request) {
-  const {data, error} = await supabase.from('countries').select('*');
-  
+  const { data, error } = await supabase.from('countries').select('*');
+
   if (!error) {
     return NextResponse.json({ message: data }, { status: 200 });
   }
