10b2f6e3f7d906077e9bba27f8ec7006995e2908
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-04 04:18:21 +0700

    feat: add component and change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 import { cn } from '@/lib/utils';
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans'
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,10 +20,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable
-        )}>{children}</body>
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
