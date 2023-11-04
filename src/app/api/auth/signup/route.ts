6f56ec648b776e5bacd82bd2d2476bf11e8be868
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-04 15:51:35 +0700

    feat: finished login and register auth

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,8 +1,27 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { supabase } from '@/lib/supabase';
+import { AuthApiError, AuthError } from '@supabase/supabase-js';
 
-export async function GET(request: Request) {
-  return NextResponse.json(
-    { message: 'This is the register route' },
-    { status: 200 }
-  );
+export async function GET(request :NextRequest) {
+  const { origin } = request.nextUrl;
+
+  return NextResponse.redirect(`${origin}/auth/register`);
+}
+
+export async function POST(request :NextRequest) {
+  const { username, email, password } = await request.json();
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: { username },
+    },
+  });
+
+  if (!error) {
+    const { origin } = request.nextUrl;
+    return NextResponse.redirect(`${origin}/auth/login`);
+  }
+  return new NextResponse(error.message, { status: error.status || 500 });
 }
