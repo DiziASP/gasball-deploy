86fe95ce462b2853b6882bd9061e1900590fc2c9
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-21 23:43:14 +0700

    feat: finished sales dashboard

--- src/app/(admin)/dashboard/(dashboards)/Sales.tsx
+++ src/app/(admin)/dashboard/(dashboards)/Sales.tsx
@@ -5,15 +5,21 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
-  Line,
-  LineChart,
-  Pie,
-  PieChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis
 } from 'recharts';
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
 
 import {
   Table,
@@ -24,138 +30,32 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table';
+
 import { useEffect, useState } from 'react';
-
-const data = [
-  {
-    name: 'January',
-    amt: 10,
-    total: 1000000
-  },
-  {
-    name: 'February',
-    amt: 20,
-    total: 2000000
-  },
-  {
-    name: 'March',
-    amt: 30,
-    total: 3000000
-  },
-  {
-    name: 'April',
-    amt: 40,
-    total: 4000000
-  },
-  {
-    name: 'May',
-    amt: 50,
-    total: 5000000
-  },
-  {
-    name: 'June',
-    amt: 70,
-    total: 7000000
-  },
-  {
-    name: 'July',
-    amt: 30,
-    total: 3000000
-  },
-  {
-    name: 'August',
-    amt: 25,
-    total: 2500000
-  },
-  {
-    name: 'September',
-    amt: 30,
-    total: 5000000
-  },
-  {
-    name: 'October',
-    amt: 70,
-    total: 5000000
-  },
-  {
-    name: 'November',
-    amt: 20,
-    total: 8000000
-  },
-  {
-    name: 'December',
-    amt: 50,
-    total: 8000000
-  }
-];
-
-const data2 = [
-  {
-    category: 'Outdoor Field',
-    value: 100
-  },
-  {
-    category: 'Indoor Field',
-    value: 300
-  }
-];
-
-const invoices: {
-  id: string;
-  date: string;
-  field: string;
-  customer: string;
-  price: number;
-}[] = [
-  {
-    id: '1',
-    date: '2021-01-01',
-    field: 'Outdoor Field',
-    customer: 'John Doe',
-    price: 1000000
-  },
-  {
-    id: '2',
-    date: '2021-01-01',
-    field: 'Outdoor Field',
-    customer: 'John Doe',
-    price: 1000000
-  },
-  {
-    id: '3',
-    date: '2021-01-01',
-    field: 'Outdoor Field',
-    customer: 'John Doe',
-    price: 1000000
-  },
-  {
-    id: '4',
-    date: '2021-01-01',
-    field: 'Outdoor Field',
-    customer: 'John Doe',
-    price: 1000000
-  },
-  {
-    id: '5',
-    date: '2021-01-01',
-    field: 'Outdoor Field',
-    customer: 'John Doe',
-    price: 1000000
-  }
-];
 
 async function getAllReservation() {
   try {
     const origin = 'http://localhost:3000';
     const res = await fetch(`${origin}/api/reservation`);
     const data = await res.json();
-    console.log(data);
+
     return data['data']['reservation'];
   } catch (err) {
     console.log(err);
   }
 }
 
+async function getReports(year: number) {
+  try {
+    const origin = 'http://localhost:3000';
+    const res = await fetch(`${origin}/api/report/${year}`);
+    const data = await res.json();
+
+    return data['data']['report'];
+  } catch (err) {
+    console.log(err);
+  }
+}
 export default function SalesDashboard() {
   const [data, setData] = useState<
     {
@@ -169,25 +69,66 @@ export default function SalesDashboard() {
       paidStatus: boolean;
       created_at: string;
       updated_at: string;
+      fields: {
+        id: string;
+        name: string;
+      };
+      users: {
+        id: string;
+        username: string;
+        full_name: string;
+      };
     }[]
   >([]);
+
+  const [reports, setReports] = useState<
+    { name: string; amt: number; total: number }[]
+  >([]);
+
+  const [year, setYear] = useState<number>(2023);
 
   useEffect(() => {
     getAllReservation().then((data) => {
       setData(data);
     });
-  }, []);
+
+    getReports(year).then((data) => {
+      setReports(data);
+    });
+  }, [year]);
 
   return (
     <div className="flex flex-col py-6 gap-6">
       <h1>Sales Dashboard</h1>
 
+      {/* Select Year */}
+      <Select onValueChange={(value) => {
+        setYear(Number(value))
+      }} defaultValue={String(year)}>
+        <SelectTrigger className='w-1/4'>
+          <SelectValue placeholder="Tahun" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {
+              [2023, 2022, 2021].map((year) => (
+                <SelectItem
+                  key={year}
+                  value={String(year)}
+                >
+                  {year}
+                </SelectItem>
+              ))
+            }
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       <div className="flex flex-row gap-3">
         <ResponsiveContainer width="100%" height={480}>
           <BarChart
             width={500}
             height={300}
-            data={data}
+            data={reports}
             margin={{
               top: 20,
               right: 30,
@@ -206,16 +147,11 @@ export default function SalesDashboard() {
               }
               width={120}
             />
-            <Tooltip
-              formatter={(value: number) =>
-                new Intl.NumberFormat('id', {
-                  style: 'currency',
-                  currency: 'IDR'
-                }).format(value)
-              }
-            />
+            <Tooltip/>
             <Legend />
             <Bar dataKey="total" stackId="a" fill="#8884d8" />
+
+            <Bar dataKey="amt" stackId="a" fill="#000000" />
           </BarChart>
         </ResponsiveContainer>
       </div>
@@ -226,22 +162,22 @@ export default function SalesDashboard() {
           <TableCaption>Recent purchases in your fields</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead className="w-[100px]"></TableHead>
               <TableHead>Date</TableHead>
               <TableHead>Field</TableHead>
-              <TableHead>PIC</TableHead>
+              <TableHead>Customer</TableHead>
               <TableHead className="text-right">Price</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data.map((invoice) => (
-              <TableRow key={invoice.customerId}>
-                <TableCell className="font-medium">
-                  {invoice.customerId}
+            {data.map((invoice, index) => (
+              <TableRow key={index + 1}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>
+                  {new Date(invoice.orderDate).toLocaleDateString('ID')}
                 </TableCell>
-                <TableCell>{invoice.orderDate}</TableCell>
-                <TableCell>{invoice.fieldId}</TableCell>
-                <TableCell>{invoice.customerName}</TableCell>
+                <TableCell>{invoice.fields.name}</TableCell>
+                <TableCell>{invoice.users.full_name}</TableCell>
                 <TableCell className="text-right">
                   {new Intl.NumberFormat('id', {
                     style: 'currency',
