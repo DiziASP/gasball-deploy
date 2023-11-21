41095d4225e3718d1f298a3cbe67bf50be526b26
fawwazabrials <18221067@std.stei.itb.ac.id>
2023-11-22 00:05:59 +0700

    feat: add keeper history page

--- src/app/(site)/(customer)/history/(histories)/KeeperHistory.tsx
+++ src/app/(site)/(customer)/history/(histories)/KeeperHistory.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Table,
   TableBody,
@@ -6,12 +8,63 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table';
+import { useEffect, useState } from 'react';
 
-async function getKeeperFields() {}
+async function getKeeperFieldReservations() {
+  const keeperId = '0de8ba4c-ce84-4374-a2a5-ecbf9776efb8'; // ini nanti ganti ke auth user
 
-async function getKeeperFieldReservations() {}
+  try {
+    const origin = 'http://localhost:3000';
+    const res = await fetch(`${origin}/api/reservation?keeperId=${keeperId}`);
+    const data = await res.json();
+    console.log(data);
+    return data['data']['reservation'];
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-export default async function KeeperHistory() {
+function extractDate(date: string) {
+  return new Date(date).toLocaleDateString();
+}
+
+function extractTime(date: string, range: number) {
+  const startTime = new Date(date).getHours(); 
+  const endTime = new Date(date).getHours()+range;
+  return `${startTime}.00 - ${endTime}.00`;
+}
+
+export default function KeeperHistory() {
+  const [data, setData] = useState<
+    {
+      id: string;
+      fieldId: string;
+      customerId: string;
+      customerName: string;
+      orderDate: string;
+      hourRange: number;
+      totalPrice: number;
+      paidStatus: boolean;
+      created_at: string;
+      updated_at: string;
+      fields: {
+        id: string;
+        name: string;
+        keeperId: string;
+      };
+      users: {
+        id: string;
+        username: string;
+        full_name: string;
+      };
+    }[]
+  >([]);
+
+  useEffect(() => {
+    getKeeperFieldReservations().then((data) => {
+      setData(data);
+    });
+  }, []);
   return (
     <div>
       <div className="grid m-20 gap-10">
@@ -29,21 +82,18 @@ export default async function KeeperHistory() {
                     <TableCell>Waktu Pemesanan</TableCell>
                   </TableRow>
                 </TableHeader>
-                {/* <TableBody>
-                  {fields.map((row) => (
+                <TableBody>
+                  {data.map((row) => (
                     <TableRow key={row.id}>
                       <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.nama_lapangan}</TableCell>
-                      <TableCell>{row.alamat}</TableCell>
-                      <TableCell>{row.keterangan}</TableCell>
-                      <TableCell>{row.penjaga_lapangan}</TableCell>
-                      <TableCell>{row.hourly_price}</TableCell>
+                      <TableCell>{row.fields.name}</TableCell>
+                      <TableCell>{extractDate(row.orderDate)}</TableCell>
                       <TableCell>
-                        <Dropdown options={options} />
+                        {extractTime(row.orderDate, row.hourRange)}
                       </TableCell>
                     </TableRow>
                   ))}
-                </TableBody> */}
+                </TableBody>
               </Table>
             </div>
           </div>
