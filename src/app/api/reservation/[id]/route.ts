c4835105c843be0025d39eea0c1c25105423b867
fawwazabrials <18221067@std.stei.itb.ac.id>
2023-11-21 15:11:00 +0700

    feat: finish backend
    - add reservation routes for CRUD and payment
    - todo: routes aren't protected yet

---
+++ src/app/api/reservation/[id]/route.ts
@@ -0,0 +1,23 @@
+import { getReservationById } from '@/services/reservations';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const id = requestUrl.pathname.split('/').pop();
+
+  const { data, error } = await getReservationById(id);
+  if (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: 'error',
+        message: error.message
+      },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { status: 'success', message: 'Reservations succesfully fetched', data },
+    { status: 200 }
+  );
+}
--- src/services/reservations.ts
+++ src/services/reservations.ts
@@ -2,8 +2,13 @@ import { createClient } from '@/lib/supabase/server';
 import { Payload } from '../../types/database.types';
 import { cookies } from 'next/headers';
 
-
-
+export interface ReservationFilter {
+  yearStart: number | null;
+  monthStart: number | null;
+  yearEnd: number | null;
+  monthEnd: number | null;
+  paidStatus: boolean | null;
+}
 
 export const createReservation = async (
   reservationPayload: Payload<'reservations'>
@@ -12,64 +17,80 @@ export const createReservation = async (
   const query = supabase
     .from('reservations')
     .insert([{ ...reservationPayload }])
+    .select();
+  const { data, error } = await query;
+  return { data, error };
+};
+
+export const getReservations = async (reservationFilter: ReservationFilter) => {
+  const { yearStart, yearEnd, monthStart, monthEnd, paidStatus } =
+    reservationFilter;
+  const supabase = createClient(cookies());
+  const query = supabase.from('reservations').select();
+
+  console.log(yearStart, monthStart, yearEnd, monthEnd);
+
+  if (paidStatus !== null) {
+    query.eq('paidStatus', paidStatus);
+  }
+  if (
+    yearStart !== null &&
+    monthStart !== null &&
+    yearEnd !== null &&
+    monthEnd !== null
+  ) {
+    const startDate = new Date(yearStart, monthStart, 1).toISOString();
+    const endDate = new Date(yearEnd, monthEnd, 1).toISOString();
+    query.lt('orderDate', endDate);
+    query.gt('orderDate', startDate);
+  }
+  const { data, error } = await query;
+  return { data, error };
+};
+
+export const getReservationById = async (id: string | null | undefined) => {
+  const supabase = createClient(cookies());
+  const query = supabase
+    .from('reservations')
     .select()
+    .eq('id', id as string)
     .single();
   const { data, error } = await query;
   return { data, error };
 };
 
-export const getReservation = async () => {
-  const supabase = createClient(cookies());
-  const query = supabase.from('reservations').select();
-
-  // filters go here
-
-  const { data, error } = await query;
-  return { data, error };
-};
-
-export const getReservationById = async (id: string) => {
-  const supabase = createClient(cookies());
-  const query = supabase.from('reservations').select().eq('id', id).single();
-  const { data, error } = await query;
-  return { data, error };
-};
-
-export const getReservationByFieldId = async (fieldId: string) => {
-  const supabase = createClient(cookies());
-  const query = supabase.from('reservations').select().eq('fieldId', fieldId);
-  const { data, error } = await query;
-  return { data, error };
-};
-
-export const getReservationByCustomerId = async (customerId: string) => {
-  const supabase = createClient(cookies());
-  const query = supabase
-    .from('reservations')
-    .select()
-    .eq('customerId', customerId);
-  const { data, error } = await query;
-  return { data, error };
-};
-
-export const updateReservation = async (
-  id: string,
-  reservationPayload: Payload<'reservations'>
+export const getReservationByFieldId = async (
+  fieldId: string | null | undefined
 ) => {
   const supabase = createClient(cookies());
   const query = supabase
     .from('reservations')
-    .update({ ...reservationPayload })
-    .eq('id', id)
     .select()
-    .single();
+    .eq('fieldId', fieldId as string);
   const { data, error } = await query;
   return { data, error };
 };
 
-export const deleteReservation = async (id: string) => {
+export const getReservationByCustomerId = async (
+  customerId: string | null | undefined
+) => {
   const supabase = createClient(cookies());
-  const query = supabase.from('reservations').delete().eq('id', id);
-  const { error } = await query;
-  return { error };
+  const query = supabase
+    .from('reservations')
+    .select()
+    .eq('customerId', customerId as string);
+  const { data, error } = await query;
+  return { data, error };
+};
+
+export const payReservation = async (id: string | null | undefined) => {
+  const supabase = createClient(cookies());
+  const query = supabase
+    .from('reservations')
+    .update({ paidStatus: true })
+    .eq('id', id as string)
+    .select()
+    .single();
+  const { data, error } = await query;
+  return { data, error };
 };
