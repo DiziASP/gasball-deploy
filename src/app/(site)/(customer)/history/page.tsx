dc9451c70a18651572f31d919f34dd61a3aecb7e
ojan <16521411@mahasiswa.itb.ac.id>
2023-11-22 17:48:18 +0700

    finished reservation history

--- src/app/(site)/(customer)/history/page.tsx
+++ src/app/(site)/(customer)/history/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Table,
   TableBody,
@@ -8,121 +9,111 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Label } from "@radix-ui/react-label";
 
-const data = [
-  {
-    "date": "2023-11-15",
-    "namaLapangan": "Lapangan Indoor ABC",
-    "waktu": "18.00-19.00",
-    "harga": "Rp 120.000",
-    "statusPembayaran": "Pending"
-  },
-  {
-    "date": "2023-11-12",
-    "namaLapangan": "Lapangan Outdoor XYZ",
-    "waktu": "19.00-20.00",
-    "harga": "Rp 150.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-11-10",
-    "namaLapangan": "Lapangan Indoor QWE",
-    "waktu": "16.00-17.00",
-    "harga": "Rp 100.000",
-    "statusPembayaran": "Rejected"
-  },
-  {
-    "date": "2023-11-08",
-    "namaLapangan": "Lapangan Outdoor ZXC",
-    "waktu": "17.00-18.00",
-    "harga": "Rp 130.000",
-    "statusPembayaran": "Pending"
-  },
-  {
-    "date": "2023-11-05",
-    "namaLapangan": "Lapangan Indoor QAZ",
-    "waktu": "20.00-21.00",
-    "harga": "Rp 110.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-11-02",
-    "namaLapangan": "Lapangan Outdoor WSX",
-    "waktu": "15.00-16.00",
-    "harga": "Rp 140.000",
-    "statusPembayaran": "Pending"
-  },
-  {
-    "date": "2023-10-30",
-    "namaLapangan": "Lapangan Indoor EDC",
-    "waktu": "18.00-19.00",
-    "harga": "Rp 120.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-10-28",
-    "namaLapangan": "Lapangan Outdoor RFV",
-    "waktu": "19.00-20.00",
-    "harga": "Rp 150.000",
-    "statusPembayaran": "Rejected"
-  },
-  {
-    "date": "2023-10-25",
-    "namaLapangan": "Lapangan Indoor TGB",
-    "waktu": "16.00-17.00",
-    "harga": "Rp 100.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-10-22",
-    "namaLapangan": "Lapangan Outdoor YHN",
-    "waktu": "17.00-18.00",
-    "harga": "Rp 130.000",
-    "statusPembayaran": "Pending"
-  },
-  {
-    "date": "2023-10-20",
-    "namaLapangan": "Lapangan Indoor UJM",
-    "waktu": "20.00-21.00",
-    "harga": "Rp 110.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-10-18",
-    "namaLapangan": "Lapangan Outdoor IKL",
-    "waktu": "15.00-16.00",
-    "harga": "Rp 140.000",
-    "statusPembayaran": "Pending"
-  },
-  {
-    "date": "2023-10-15",
-    "namaLapangan": "Lapangan Indoor OPN",
-    "waktu": "18.00-19.00",
-    "harga": "Rp 120.000",
-    "statusPembayaran": "Verified"
-  },
-  {
-    "date": "2023-10-12",
-    "namaLapangan": "Lapangan Outdoor JKL",
-    "waktu": "19.00-20.00",
-    "harga": "Rp 150.000",
-    "statusPembayaran": "Rejected"
-  },
-  {
-    "date": "2023-10-10",
-    "namaLapangan": "Lapangan Indoor MNB",
-    "waktu": "16.00-17.00",
-    "harga": "Rp 100.000",
-    "statusPembayaran": "Verified"
+import { useEffect, useState } from "react";
+
+type Reservation = {
+  id: string;
+  fieldId: string;
+  customerId: string;
+  customerName: string;
+  orderDate: string;
+  hourRange: number;
+  totalPrice: number;
+  paidStatus: boolean;
+  created_at: string;
+  updated_at: string;
+  fields: {
+    id: string;
+    name: string;
+    keeperId: string;
+  };
+  users: {
+    id: string;
+    username: string;
+    full_name: string;
   }
-]
+};
 
+
+async function getReservationHistory(customerId: string){
+  try {
+    const apiUrl = "http://localhost:3000/api/reservation";
+    const query = `?customerId=${customerId}`;
+    const response = await fetch(apiUrl + query, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    } 
+    const res = await response.json();
+    console.log('Fetched reservation data:', res);
+
+    const reservationData : Reservation[] = res.data.reservation;
+    return reservationData;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getSelf(){
+  try {
+    const apiUrl = `http://localhost:3000/api/auth/self`;
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const res = await response.json();
+    console.log('Fetched user data:', res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function dateFormat(date: string){
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth() + 1;
+  const year = dateObj.getFullYear();
+  const hour = dateObj.getHours();
+  const minute = dateObj.getMinutes();
+  return `${day}/${month}/${year} ${hour}:${minute}`;
+}
 
 /**
  * Halaman Riwayat Pemesanan Pelanggan
  * @returns The purchase history page component.
  */
-export default async function PurchaseHistory() {
+export default function PurchaseHistory() {
+
+  const [tableData, setTableData] = useState<Reservation[]>([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const self = await getSelf();
+      const reservationHistory = await getReservationHistory(self.data.user.id);
+      setTableData(reservationHistory || []); // Provide an empty array as fallback
+      setUser(self);
+    }
+    fetchData();
+  }, []);
+
+  
+
+
+
   return (
     <div className="grid bg-gradient-to-r from-[#FDFEFF] to-[#ECF4FF]">
       <div className="grid m-20 gap-10">
@@ -131,7 +122,7 @@ export default async function PurchaseHistory() {
           <div className="m-10">  
             <div>
               <Table>
-                <TableCaption>Daftar Riwayat Pemesanan</TableCaption>
+                { tableData && tableData.length === 0 ? <TableCaption>Belum ada pemesanan</TableCaption> : <></> } 
                 <TableHeader>
                   <TableRow>
                     <TableCell>Tanggal</TableCell>
@@ -142,13 +133,24 @@ export default async function PurchaseHistory() {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {data.map((row) => (
-                    <TableRow key={row.date}>
-                      <TableCell>{row.date}</TableCell>
-                      <TableCell>{row.namaLapangan}</TableCell>
-                      <TableCell>{row.waktu}</TableCell>
-                      <TableCell>{row.harga}</TableCell>
-                      <TableCell>{row.statusPembayaran}</TableCell>
+                  {tableData.map((row) => (
+                    <TableRow key={row.orderDate}>
+                      <TableCell>{dateFormat(row.orderDate)}</TableCell>
+                      <TableCell>{row.fields.name}</TableCell>
+                      <TableCell>{row.hourRange}</TableCell>
+                      <TableCell>{row.totalPrice}</TableCell>
+                      <TableCell>
+                        {
+                          row.paidStatus ? 
+                          <div className="flex w-fit p-1 rounded-[5px] bg-green-100">
+                            <Label className="text-green-800">Berhasil</Label>
+                          </div>
+                          :
+                          <div className="flex w-fit p-1 rounded-[5px] bg-red-100">
+                            <Label className="text-red-800">Belum terverifkasi</Label>
+                          </div>
+                        }
+                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
