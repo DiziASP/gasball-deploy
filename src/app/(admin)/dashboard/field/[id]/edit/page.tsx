3c15cb532938032dd76b55ac4a29ebd4a07ee6a1
nauravalda <16521058@mahasiswa.itb.ac.id>
2023-11-22 19:15:23 +0700

    feat: blm update sm tambah lapangan

--- src/app/(admin)/dashboard/field/[id]/edit/page.tsx
+++ src/app/(admin)/dashboard/field/[id]/edit/page.tsx
@@ -12,46 +12,57 @@ import {
   FormField
 } from '@/components/ui/form';
 
+async function getDetailField(id: string) {
+  try {
+    const origin = 'http://localhost:3000';
+    const res = await fetch(`${origin}/api/field/${id}`);
+    const data = await res.json();
+
+    return data['data']['field'];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import RadioGroup from '@/components/ui/radiogroup';
 import CheckBox from '@/components/ui/checkbox';
 
-export default async function AddField() {
+export default async function AddField({
+  params
+}: {
+  params: { id: string };
+}) {
+
   const [selectedOption, setSelectedOption] = useState(''); // State untuk nilai terpilih
 
   const handleOptionChange = (selected: string) => {
     return selected;
   };
+  const field = await getDetailField(params.id);
   return (
     <div className="flex flex-row justify-center w-full p-10 bg-gradient-to-r from-[#FDFEFF] to-[#ECF4FF]">
       <div className="flex w-10/12 items-start gap-[25px] p-[25px] relative bg-[#ffffff] rounded-[10px] shadow-2xl">
         <div className="flex-col items-start gap-[25px] flex-1 grow flex relative">
-          <div className="flex-col h-[103px] items-center justify-center gap-[25px] p-[50px] self-stretch w-full bg-[#ffffff] rounded-[27px] overflow-hidden border border-dashed border flex relative">
-            <Button
-              className="!self-stretch !mt-[-18.50px] !mb-[-18.50px] !flex-[0_0_auto] !flex !w-full"
-              variant="secondary"
-            >
-              Upload picture
-            </Button>
-          </div>
+          
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
               <Label>Nama Lapangan</Label>
-              <Input type="name" placeholder="Masukkan nama lapangan" />
+              <Input type="name" placeholder={field.name} />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
-              <Label>Deskripsi Lapangan</Label>
-              <Input type="text" placeholder="Masukkan deskripsi lapangan" />
+              <Label>Lokasi Lapangan</Label>
+              <Input type="text" placeholder={field.location} />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
-              <Label>Alamat Lapangan</Label>
-              <Input type="desc" placeholder="Masukkan alamat lapangan" />
+              <Label>Harga/Jam</Label>
+              <Input type="number" placeholder={field.pricePerHour} />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
@@ -63,8 +74,6 @@ export default async function AddField() {
               <Label>Jenis Rumput</Label>
               <RadioGroup
                 options={['Rumput sintetis', 'Rumput Alami']}
-                // selectedOption={selectedOption}
-                // onChange={handleOptionChange}
               ></RadioGroup>
             </div>
             <div className="grid w-full items-center gap-1.5">
