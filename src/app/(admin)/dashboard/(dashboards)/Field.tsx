8d871ae762ebb5cf6d8ca9f5ee7dfeb6e24bb1db
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-11-24 16:02:00 +0700

    feat: dahlah

--- src/app/(admin)/dashboard/(dashboards)/Field.tsx
+++ src/app/(admin)/dashboard/(dashboards)/Field.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Table,
   TableBody,
@@ -8,153 +10,148 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table';
-import React from 'react';
-import Dropdown from '@/components/ui/dropdown';
+import React, { useState, useEffect } from 'react';
+import Dropdown from '@/components/ui/dropdown-menu';
 import Link from 'next/link';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { TrashIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
-const fields = [
-  {
-    id: 1,
-    nama_lapangan: 'Futsal Arena A',
-    alamat: 'Jalan Merdeka No. 10',
-    keterangan: 'Lapangan berukuran standar untuk turnamen lokal.',
-    penjaga_lapangan: 'budi123',
-    hourly_price: 100000
-  },
-  {
-    id: 2,
-    nama_lapangan: 'Soccer Field X',
-    alamat: 'Jalan Pahlawan No. 25',
-    keterangan: 'Lapangan sintetis dengan pemandangan kota.',
-    penjaga_lapangan: 'anto456',
-    hourly_price: 120000
-  },
-  {
-    id: 3,
-    nama_lapangan: 'Futsal Zone B',
-    alamat: 'Jalan Jenderal Sudirman No. 8',
-    keterangan: 'Lapangan dengan pencahayaan bagus.',
-    penjaga_lapangan: 'agus789',
-    hourly_price: 90000
-  },
-  {
-    id: 4,
-    nama_lapangan: 'Futsal Square',
-    alamat: 'Jalan Gatot Subroto No. 14',
-    keterangan: 'Lapangan indoor dengan AC.',
-    penjaga_lapangan: 'suhardi123',
-    hourly_price: 150000
-  },
-  {
-    id: 5,
-    nama_lapangan: 'Goal Futsal Center',
-    alamat: 'Jalan Diponegoro No. 30',
-    keterangan: 'Lapangan dengan fasilitas kamar mandi bersih.',
-    penjaga_lapangan: 'hendro321',
-    hourly_price: 110000
-  },
-  {
-    id: 6,
-    nama_lapangan: 'Green Field Futsal',
-    alamat: 'Jalan Asia Afrika No. 5',
-    keterangan: 'Lapangan di tengah taman dengan alam yang hijau.',
-    penjaga_lapangan: 'joko567',
-    hourly_price: 95000
-  },
-  {
-    id: 7,
-    nama_lapangan: 'Futsal Paradise',
-    alamat: 'Jalan Thamrin No. 20',
-    keterangan: 'Lapangan dengan area penonton yang luas.',
-    penjaga_lapangan: 'hadi890',
-    hourly_price: 130000
-  },
-  {
-    id: 8,
-    nama_lapangan: 'Soccer Center',
-    alamat: 'Jalan Imam Bonjol No. 12',
-    keterangan: 'Lapangan yang terletak di pusat kota.',
-    penjaga_lapangan: 'eko234',
-    hourly_price: 85000
-  },
-  {
-    id: 9,
-    nama_lapangan: 'Futsal Galaxy',
-    alamat: 'Jalan Cendrawasih No. 17',
-    keterangan: 'Lapangan dengan mural seni di dinding.',
-    penjaga_lapangan: 'rudi567',
-    hourly_price: 100000
-  },
-  {
-    id: 10,
-    nama_lapangan: 'Victory Futsal Park',
-    alamat: 'Jalan Veteran No. 7',
-    keterangan: 'Lapangan dengan fasilitas cuci sepatu gratis.',
-    penjaga_lapangan: 'agus123',
-    hourly_price: 120000
-  },
-  {
-    id: 11,
-    nama_lapangan: 'Futsal Dreamland',
-    alamat: 'Jalan Arjuna No. 22',
-    keterangan: 'Lapangan dengan desain modern.',
-    penjaga_lapangan: 'bambang890',
-    hourly_price: 110000
-  },
-  {
-    id: 12,
-    nama_lapangan: 'Goalkeeper Arena',
-    alamat: 'Jalan Kusuma Bangsa No. 13',
-    keterangan: 'Lapangan dengan gawang-gawang yang berkualitas.',
-    penjaga_lapangan: 'david456',
-    hourly_price: 125000
-  },
-  {
-    id: 13,
-    nama_lapangan: 'Futsal United',
-    alamat: 'Jalan Pelita No. 28',
-    keterangan: 'Lapangan dengan suhu ruangan yang nyaman.',
-    penjaga_lapangan: 'heru678',
-    hourly_price: 95000
-  },
-  {
-    id: 14,
-    nama_lapangan: 'Futsal World',
-    alamat: 'Jalan Kartini No. 9',
-    keterangan: 'Lapangan dengan tema permainan bola dunia.',
-    penjaga_lapangan: 'surya789',
-    hourly_price: 105000
-  },
-  {
-    id: 15,
-    nama_lapangan: 'Soccer Star',
-    alamat: 'Jalan Tanjung Duren No. 4',
-    keterangan: 'Lapangan favorit untuk latihan tim lokal.',
-    penjaga_lapangan: 'rudi987',
-    hourly_price: 90000
+async function getAllField() {
+  try {
+    const res = await fetch(`$/api/field`);
+    const data = await res.json();
+
+    return data['data']['field'];
+  } catch (err) {
+    console.log(err);
   }
-];
+}
 
-export default async function FieldManagement() {
-  const options = [
-    {
-      label: 'Edit detail lapangan',
-      link: 'dashboard/field/01/edit'
-    },
-    {
-      label: 'Lihat riwayat penggunaan lapangan',
-      link: 'dashboard/field/01/history'
-    }
-  ];
+interface DeleteConfirmationPopupProps {
+  onDelete: () => void;
+  onCancel: () => void;
+  id : string;
+  name : string;
+  location : string;
+  syntheticGrass : boolean;
+  indoor : boolean;
+  playerBench : boolean;
+  watcherBench : boolean;
+}
+
+async function deleteField(id: string) {
+  try {
+    const res = await fetch(`/api/field/${id}`, {
+      method: 'DELETE'
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
+  onDelete,
+  onCancel,
+  id,
+  name,
+  location,
+  syntheticGrass,
+  indoor,
+  playerBench,
+  watcherBench
+}) => {
   return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="gap-5 bg-white w-3/4 h-2/3 p-20 m-20 rounded-3xl block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+        <h2 className='p-5 text-center' >Konfirmasi hapus lapangan</h2>
+        <div className="flex pt-10 px-10 gap-4">
+          <Image
+                    src="/assets/images/field.jpg"
+                    alt=""
+                    width={180}
+                    height={280}
+                  />
+          <div className='flex-col justify-center'>
+            <h3 className="h-24"> {name}</h3>
+            <h4 className="pt-2"> {location}</h4>
+            <p className='font-medium text-base'> {syntheticGrass ? 'Rumput Sintentis' : 'Rumput Alami'}
+            {indoor ? ' | Indoor' : ' | Outdoor'
+            } {playerBench && '| Bench Pemain'} {watcherBench && '| Bench Penonton'
+            } </p>
+          </div>
+        </div>
+        <div className='flex w-full px-10 pt-10 gap-5 items-center  justify-center'>
+        <Button
+              className="w-3/4 h-14 rounded-xl"
+              type="submit"
+              variant={'destructive'}
+              onClick={onDelete}
+            >
+              Hapus
+            </Button>
+            <Button
+              className=" w-3/4 h-14 rounded-xl"
+              variant={'outline'}
+              onClick={onCancel}
+            >
+              Batal
+            </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function FieldManagement() {
+  const [fields, setFields] = useState([]);
+
+  // Fungsi untuk mengambil data lapangan dari API
+  const fetchData = async () => {
+    const data = await getAllField();
+    setFields(data);
+  };
+
+  // Fungsi polling untuk mengambil data setiap 5 detik
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData();
+    }, 500); // Interval polling setiap 5 detik (5000 milidetik)
+
+    // Bersihkan interval saat komponen tidak lagi ter-render
+    return () => clearInterval(interval);
+  }, []); // Jalankan sekali saat komponen ter-render
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleDelete = () => {
+    deleteField(String(selectedField));
+    setShowPopup(false);
+  };
+
+  const handleCancel = () => {
+    // Batalkan tindakan hapus dan sembunyikan pop-up
+    setShowPopup(false);
+  };
+  const [selectedField, setSelectedField] = useState('');
+  const handleShowPopup = (fieldId: string) => {
+    setSelectedField(fieldId);
+    setShowPopup(true);
+  };
+
+  return (
+    
     <div>
-      <div className="grid m-20 gap-10">
-        <div className="flex justify-between">
+      <div className="grid m-10 gap-10">
+        <div className="flex inline-flex justify-between">
           <h1>Data Lapangan</h1>
           <Link href={`dashboard/field/add-field`}>
             <button
               className={
-                'h-[30px] self-end inline-flex items-center gap-[10px] px-[10px] py-[5px] relative bg-[#ffffff] hover:bg-slate-100 rounded-[6px] shadow-[0px_2px_4px_#1e293b40]'
+                'h-full self-end inline-flex items-center gap-[10px] px-[10px] py-[5px] relative bg-[#ffffff] hover:bg-slate-100 rounded-[6px] shadow-[0px_2px_4px_#1e293b40]'
               }
             >
               <div className="relative w-fit mt-[-1.00px] font-medium text-[#000000] text-[14px] tracking-[0] leading-[20px] whitespace-nowrap">
@@ -169,30 +166,75 @@ export default async function FieldManagement() {
               <Table>
                 <TableCaption>Data Lapangan</TableCaption>
                 <TableHeader>
-                  <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>Nama Lapangan</TableCell>
-                    <TableCell>Alamat</TableCell>
-                    <TableCell>Keterangan</TableCell>
+                  <TableRow className="font-medium">
+                    <TableCell>Nama</TableCell>
+                    <TableCell>Lokasi</TableCell>
+                    <TableCell>Fasilitas</TableCell>
                     <TableCell>Penjaga Lapangan</TableCell>
                     <TableCell>Harga/Jam</TableCell>
                     <TableCell></TableCell>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {fields.map((row) => (
-                    <TableRow key={row.id}>
-                      <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.nama_lapangan}</TableCell>
-                      <TableCell>{row.alamat}</TableCell>
-                      <TableCell>{row.keterangan}</TableCell>
-                      <TableCell>{row.penjaga_lapangan}</TableCell>
-                      <TableCell>{row.hourly_price}</TableCell>
-                      <TableCell>
-                        <Dropdown options={options} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                  {fields.map(
+                    (row: {
+                      id: React.Key | null | undefined;
+                      name:
+                        | string
+                        | undefined;
+                      location:
+                        | string
+                        
+                        | undefined;
+                      syntheticGrass: any;
+                      Indoor: any;
+                      playerBench: any;
+                      watcherBench: any;
+                      users: {
+                        full_name:
+                          | string
+                         
+                          | undefined;
+                      };
+                      pricePerHour: { toString: () => string };
+                    }) => (
+                      <TableRow key={row.id}>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell>{row.location}</TableCell>
+                        <TableCell>
+                          {row.syntheticGrass
+                            ? 'Rumput Sintentis'
+                            : 'Rumput Alami'}{' '}
+                          {row.Indoor ? '| Indoor' : '| Outdoor'}{' '}
+                          {row.playerBench && '| Bench Pemain'}{' '}
+                          {row.watcherBench && '| Bench Penonton'}
+                        </TableCell>
+                        <TableCell>{row.users.full_name}</TableCell>
+                        <TableCell>
+                          Rp{' '}
+                          {row.pricePerHour
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}{' '}
+                          ,00
+                        </TableCell>
+                        <TableCell>
+                          <Link href={`dashboard/field/${row.id}/edit`}>
+                            <button >
+                              <Pencil2Icon />
+                            </button>
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                            <button onClick={() => handleShowPopup(String(row.id))}>
+                              <TrashIcon />
+                            </button>
+                          {showPopup && selectedField===row.id && (
+        <DeleteConfirmationPopup onDelete={handleDelete} onCancel={handleCancel} name={row.name} id={row.id} location={row.location}  syntheticGrass={row.syntheticGrass} indoor={row.indoor} playerBench={row.playerBench} watcherBench={row.watcherBench}/>
+      )}
+                        </TableCell>
+                      </TableRow>
+                    )
+                  )}
                 </TableBody>
               </Table>
             </div>
--- src/app/(admin)/dashboard/field/[id]/edit/page.tsx
+++ src/app/(admin)/dashboard/field/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   useFormField,
   Form,
@@ -17,60 +17,195 @@ import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import RadioGroup from '@/components/ui/radiogroup';
 import CheckBox from '@/components/ui/checkbox';
+import { useRouter } from 'next/navigation';
 
-export default async function AddField() {
+async function getDetailField(id: string) {
+  try {
+    const res = await fetch(`/api/field/${id}`);
+    const data = await res.json();
+
+    return data['data']['field'];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default function EditField({ params }: { params: { id: string } }) {
+  const router = useRouter();
+  const [field, setField] = useState<{
+    id: string;
+    name: string;
+    location: string;
+    pricePerHour: number;
+    syntheticGrass: boolean;
+    indoor: boolean;
+    playerBench: boolean;
+    watcherBench: boolean;
+    available: boolean;
+    user: {
+      id: string;
+      full_name: string;
+      phone_number: string;
+    };
+  }>();
+
   const [selectedOption, setSelectedOption] = useState(''); // State untuk nilai terpilih
 
   const handleOptionChange = (selected: string) => {
     return selected;
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDetailField(params.id);
+      setField(data);
+    };
+    fetchData();
+  }, []);
+
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [pricePerHour, setPricePerHour] = useState(0);
+  const [syntheticGrass, setSyntheticGrass] = useState<boolean>(false);
+  const [indoor, setIndoor] = useState<boolean>(false);
+  const [playerBench, setPlayerBench] = useState<boolean>(false);
+  const [watcherBench, setWatcherBench] = useState<boolean>(false);
+  const [available, setAvailable] = useState<boolean>(false);
+  const [keeperId, setkeeperId] = useState('');
+
+  const handleSubmit = async () => {
+    const res = await fetch(`/api/field/${params.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        keeperId,
+        name,
+        location,
+        pricePerHour,
+        syntheticGrass,
+        indoor,
+        playerBench,
+        watcherBench,
+        available
+      })
+    });
+    const data = await res.json();
+    console.log(data);
+    router.back();
+    console.log(
+      name,
+      location,
+      pricePerHour,
+      syntheticGrass,
+      indoor,
+      playerBench,
+      watcherBench,
+      available,
+      keeperId
+    );
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleLocationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(e.target.value);
+  };
+  const handlePricePerHourChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPricePerHour(e.target.value);
+  };
+  const handleKeeperIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setkeeperId(e.target.value);
+  };
+
+  const handleIndoorChange = (option: string) => {
+    setIndoor(option === 'Indoor');
+  };
+
+  const handleSyntheticGrassChange = (option: string) => {
+    setSyntheticGrass(option === 'Rumput sintetis');
+  };
+
+  const handlePlayerBenchChange = (isChecked: boolean) => {
+    setPlayerBench(isChecked);
+  };
+
+  const handleWatcherBenchChange = (isChecked: boolean) => {
+    setWatcherBench(isChecked);
+  };
+
+  const handleAvailableChange = (option: string) => {
+    setAvailable(option === 'Available');
+  };
+
   return (
     <div className="flex flex-row justify-center w-full p-10 bg-gradient-to-r from-[#FDFEFF] to-[#ECF4FF]">
       <div className="flex w-10/12 items-start gap-[25px] p-[25px] relative bg-[#ffffff] rounded-[10px] shadow-2xl">
         <div className="flex-col items-start gap-[25px] flex-1 grow flex relative">
-          <div className="flex-col h-[103px] items-center justify-center gap-[25px] p-[50px] self-stretch w-full bg-[#ffffff] rounded-[27px] overflow-hidden border border-dashed border flex relative">
-            <Button
-              className="!self-stretch !mt-[-18.50px] !mb-[-18.50px] !flex-[0_0_auto] !flex !w-full"
-              variant="secondary"
-            >
-              Upload picture
-            </Button>
-          </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
               <Label>Nama Lapangan</Label>
-              <Input type="name" placeholder="Masukkan nama lapangan" />
+              <Input
+                type="name"
+                placeholder={field?.name}
+                onChange={handleNameChange}
+              />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
-              <Label>Deskripsi Lapangan</Label>
-              <Input type="text" placeholder="Masukkan deskripsi lapangan" />
+              <Label>Lokasi Lapangan</Label>
+              <Input
+                type="text"
+                placeholder={field?.location}
+                onChange={handleLocationChange}
+              />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
-              <Label>Alamat Lapangan</Label>
-              <Input type="desc" placeholder="Masukkan alamat lapangan" />
+              <Label>Harga/Jam</Label>
+              <Input
+                type="number"
+                placeholder={field?.pricePerHour}
+                onChange={handlePricePerHourChange}
+              />
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
             <div className="grid w-full items-center gap-1.5">
               <Label>Jenis Lapangan</Label>
-              <RadioGroup options={['Indoor', 'Outdoor']}></RadioGroup>
+              <RadioGroup
+                options={['Indoor', 'Outdoor']}
+                onChange={handleIndoorChange}
+              ></RadioGroup>
             </div>
             <div className="grid w-full items-center gap-1.5">
               <Label>Jenis Rumput</Label>
               <RadioGroup
                 options={['Rumput sintetis', 'Rumput Alami']}
-                // selectedOption={selectedOption}
-                // onChange={handleOptionChange}
+                onChange={handleSyntheticGrassChange}
               ></RadioGroup>
             </div>
             <div className="grid w-full items-center gap-1.5">
               <Label>Fasilitas Lapangan</Label>
-              <CheckBox label="Bench Pemain"></CheckBox>
-              <CheckBox label="Bench Penonton"></CheckBox>
+              <CheckBox
+                label="Bench Pemain"
+                onChange={handlePlayerBenchChange}
+              ></CheckBox>
+              <CheckBox
+                label="Bench Penonton"
+                onChange={handleWatcherBenchChange}
+              ></CheckBox>
+            </div>
+            <div className="grid w-full items-center gap-1.5">
+              <Label>Status Lapangan</Label>
+              <RadioGroup
+                options={['Available', 'Unavailable']}
+                onChange={handleAvailableChange}
+              ></RadioGroup>
             </div>
           </div>
           <div className="items-center gap-[15px] self-stretch w-full flex-[0_0_auto] flex relative">
@@ -78,7 +213,8 @@ export default async function AddField() {
               <Label>Penjaga Lapangan</Label>
               <Input
                 type="desc"
-                placeholder="Masukkan username penjaga lapangan"
+                placeholder="Masukkan id penjaga lapangan (uu-id)"
+                onChange={handleKeeperIdChange}
               />
             </div>
           </div>
@@ -87,10 +223,14 @@ export default async function AddField() {
             <Button
               className="!self-stretch !flex-[0_0_auto] !flex !w-full"
               type="submit"
+              onClick={handleSubmit}
             >
               Simpan
             </Button>
-            <Button className="!self-stretch !flex-[0_0_auto] !flex !w-full">
+            <Button
+              className="!self-stretch !flex-[0_0_auto] !flex !w-full"
+              onClick={() => router.back()}
+            >
               Batal
             </Button>
           </div>
